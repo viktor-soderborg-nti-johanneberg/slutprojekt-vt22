937afa744413ef93da137675177822c5eef64a81
viktor-soderborg-nti-johanneberg <74763822+viktor-soderborg-nti-johanneberg@users.noreply.github.com>
2022-05-12 16:05:11 +0200

    fix game loop

--- src/App.svelte
+++ src/App.svelte
@@ -1,36 +1,180 @@
 <script>
-  import { Router, Route, Link } from "svelte-navigator";
-  import Home from './Home.svelte'
-  import Game from './Game.svelte'
-  import End from './End.svelte'
+  
+  let score = 0;
+  let highscore = 0;
+  let lastOption;
+  let currentOption;
+  let options = [];
+  
+  async function loadData(url) {
+    try {      
+      let resp = await fetch(url);
+
+      let data = await resp.json();
+
+      return data.ParkingAreaNewList
+    } catch (error) {
+      console.log(error) 
+    }
+  }
+  
+  function generateOptions() {
+    if (lastOption === undefined || lastOption.Name === "") {
+      lastOption = options[Math.floor(Math.random() * options.length)]
+      currentOption = options[Math.floor(Math.random() * options.length)]
+
+      if (lastOption === currentOption) {
+        generateOptions()
+      } else if (currentOption.ParkingSpaces === undefined) {
+        generateOptions()
+      } else if (lastOption.ParkingSpaces === undefined) {
+        generateOptions()
+      } else if (lastOption.ParkingSpaces === currentOption.ParkingSpaces) {
+        generateOptions()
+      }
+    } else {
+      lastOption = currentOption
+      currentOption = options[Math.floor(Math.random() * options.length)]
+
+      if (currentOption === lastOption) {
+        generateOptions()
+      } else if (currentOption.ParkingSpaces === undefined) {
+        generateOptions()
+      } else if (lastOption.ParkingSpaces === currentOption.ParkingSpaces) {
+        generateOptions()
+      }
+    }
+    
+    console.log(lastOption)
+    console.log(currentOption)
+  }
+  
+  function checkAnswer(answer) {
+    if (answer == 'higher' && currentOption.ParkingSpaces > lastOption.ParkingSpaces) {
+      score++
+      generateOptions();
+    } else if (answer == 'higher' && currentOption.ParkingSpaces < lastOption.ParkingSpaces) {
+      gameOver();
+    } else if (answer == 'lower' && currentOption.ParkingSpaces < lastOption.ParkingSpaces) {
+      score++
+      generateOptions();
+    } else {
+      gameOver();
+    }
+  }
+  
+  function gameOver() {
+    lastOption.Name = "";
+    if (score > highscore) {
+      highscore = score;
+    }
+    alert(`You lost! Your final score was ${score}! Close this to restart`);
+    score = 0;
+    generateOptions();
+  }
+  
+  async function main() {
+    let data = await loadData('http://parkering.linkoping.se/Parkeringsdata/ParkeringsdataV1.svc/GetParkeringsytaList/f7e5d2afb1dc41bb97e9118b592c0040/0');
+    options = [...data];
+    generateOptions();
+  }
 </script>
 
-<Router>
-
-  <main>
-    <Route path="/">
-      <Home />
-    </Route>
-
-    <Route path="/game">
-      <Game />
-    </Route>
-
-    <Route path="/end">
-      <End />
-    </Route>
-
-  </main>
-</Router>
+<main>
+  {#await main()}
+    <p>Waiting for data...</p>
+    {:then}
+      <section id="opt1">
+        <h1>{lastOption.Name}</h1>
+        <h2>{lastOption.ParkingSpaces}</h2>
+        <h3>Highscore: {highscore}</h3>
+      </section>
+      <section id="opt2">
+        <h1>{currentOption.Name}</h1>
+        <button class="answer" on:click|preventDefault={() => checkAnswer('higher')}>Higher</button>
+        <button class="answer" on:click|preventDefault={() => checkAnswer('lower')}>Lower</button>
+        <h3>Score: {score}</h3>
+      </section>
+  {/await}
+</main>
 
 <style lang="scss">
 
+  $margin: 1rem;
+  
+  @mixin center {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    gap: 1rem;
+  }
+
   main {
-    height: 100vh;
-    width: 100vw;
+    min-height: 100vh;
+    height: 100%;
+    min-width: 100vw;
+    display: grid;
+    grid-template-columns: repeat(2, 50vw);
+    grid-template-areas: 
+    'opt1 opt2';
     background-image: url("https://browsecat.net/sites/default/files/mountain-sunset-hd-2021-wallpapers-42341-48465-3645837.png");
     overflow: hidden;
     background-size: cover;
   }
+  
+  #opt1 {
+    grid-area: opt1;
+    @include center;
+    border: 1px solid black;
 
+    h3 {
+      margin-left: $margin;
+      margin-right: auto;
+    }
+    
+    h2 {
+      color: white;
+    }
+  }
+  
+  #opt2 {
+    grid-area: opt2;
+    @include center;
+    border: 1px solid black;
+    
+    h3 {
+      margin-right: $margin;
+      margin-left: auto;
+    }
+
+    button {
+      transition: all .5s ease;
+      border: 3px solid white;
+      color: yellow;
+      padding: 15px 32px;
+      text-align: center;
+      text-decoration: none;
+      font-size: 16px;
+      cursor: pointer;
+      border-radius: 2rem;
+      text-transform: uppercase;
+      background-color : transparent;
+    }
+
+  }
+
+  #opt1 h3, #opt2 h3 {
+    align-self: flex-end;
+    margin-top: auto;
+  }
+
+  #opt1 h1, #opt2 h1 {
+    margin-top: auto;
+    text-align: center;
+  }
+
+  p, #opt1 h1, #opt2 h1, #opt1 h3, #opt2 h3 {
+      color: white;
+  }
 </style>
